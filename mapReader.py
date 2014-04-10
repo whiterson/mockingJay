018c7999b1b6d5687a1aed9700b40395e554ba3e
Whitney H <henderson.whit@gmail.com>
2014-04-10 01:32:00 -0400

    Fixed Switchcase

--- mapReader.py
+++ mapReader.py
@@ -4,100 +4,108 @@ import sys
 import PIL
 import pygame
 from PIL import Image
-import numpy
 
+
+def deepForest():
+    testFile = open('maptest.txt', 'a')
+    testFile.write('Pixel ID DeepForest\n')
+    testFile.close()
 
 def dirt():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID Dirt\n')
     testFile.close()
 
 def grass():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID Grass\n')
     testFile.close()
 
 def ice():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID Ice\n')
     testFile.close()
 
 def lightForest():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID lightForest\n')
     testFile.close()
 
 def lowVegetation():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID lowVegetation\n')
     testFile.close()
 
 def mud():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID Mud\n')
     testFile.close()
 
 def rock():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID Rock\n')
     testFile.close()
 
 def sand():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID Sand\n')
     testFile.close()
 
 def shallowWater():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID shallowWater\n')
     testFile.close()
 
 def snow():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID snow\n')
     testFile.close()
 
 def swimmingWater():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID swimmingWater\n')
     testFile.close()
 
 def tallGrass():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID tallGrass\n')
     testFile.close()
 
 def wadingWater():
     testFile = open('maptest.txt', 'a')
-    testFile.write('Pixel ID Dirt')
+    testFile.write('Pixel ID wadingWater\n')
     testFile.close()
 
 """My Python Switch Case Based on Pixel Value"""
 """I have terrains stored, and I was going to get the pixel value of each color, and use them
     as the switch case values. Because Python isn't working though, I haven't done that yet. So everything's
     set up, I just need the switch case stuff. Blargh."""
-switch = {0 : deepForest,
-                1 : dirt,
-                4 : grass,
-                9 : ice,
-                2 : lightForest,
-                3 : lowVegetation,
-                5 : mud,
-                7 : rock,
-                8 : sand,
-                9 : shallowWater,
-                10: snow,
-                11: swimmingWater,
-                12: tallGrass,
-                13: wadingWater
+switch = {(1, 35, 18) : deepForest,
+          (97, 63, 2) : dirt,
+          (70, 152, 18) : grass,
+          (233, 244, 248) : ice,
+          (2, 117, 62) : lightForest,
+          (104, 142, 65) : lowVegetation,
+          (58, 43, 20) : mud,
+          (155, 154, 150) : rock,
+          (246, 238, 176) : sand,
+          (184, 224, 236) : shallowWater,
+          (247, 249, 248) : snow,
+          (38, 100, 121): swimmingWater,
+          (169, 153, 18): tallGrass,
+          (88, 156, 179): wadingWater
 }
 
-def readMap():
-    testFile = open('maptest.txt', 'a')
-    testFile.write('=============================\nNew Map Test\n=============================\n\n')
-    map = Image.open('terrains/deepForest.jpg')
-    pixelMap = map.load()
-    for pixel in pixelMap:
-        """This is where the terrain arguments will go"""
-        switch[pixel]()
-    testFile.close()
+"""def readMap():"""
+testFile = open('maptest.txt', 'a')
+testFile.write('\n\n=============================\nNew Map Test\n=============================\n\n')
+map = Image.open('terrains/wadingWater.jpg')
+pixelMap = map.load()
+for i in range(map.size[0]):    # for every pixel:
+    for j in range(map.size[1]):
+        pixel = pixelMap[i,j]
+        if pixel in switch:
+            switch[pixel]()
+        else:
+            testFile.write("Color Error\n")
+testFile.close()
