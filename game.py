6180f53907411adb3f023c6e91fcf8b33fc7ae3f
Nathan Korzekwa <ciscprocess@gmail.com>
2014-04-10 00:45:44 -0400

    Cleaned stuff up and got basic graphics working.

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 __author__ = 'Nathan'
 
+import random as r
+
 
 class GameState(object):
     """
@@ -7,10 +9,16 @@ class GameState(object):
     how should we separate the line between the map and the game state?
     for now, I'm making the crude assumption that the map read parses to an empty game-state
     """
-    def __init__(self):
-        self.map = {
+    def __init__(self, width=50, height=50):
+        # for now, this is just a dummy class with a 10x10 tile grid
+        self.world = {
             'ground': (),
             'particle': ()
         }
 
+        self.grid = {
+            'ground': [(x, y, (r.randint(0, 255), r.randint(0, 255), r.randint(0, 255), 0)) for x in range(width) for y in range(height)],
+            'particle': []
+        }
 
+        self.particles = []
--- tribute.py
+++ tribute.py
@@ -1,8 +1,16 @@
 import random
-class Tribute:
+
+
+class Particle(object):
+    def __init__(self, x=0, y=0, width=1, height=1):
+        pass
+
+
+class Tribute(Particle):
     #Goals = list of goals for tribute
     #actions = list of possible actions tribute can do
-    def __init__(self, goals, actions):
+    def __init__(self, goals, actions, x=0, y=0):
+        Particle.__init__(self, x, y, 1, 1)
         self.goals = goals
         self.actions = actions
 
@@ -10,5 +18,5 @@ class Tribute:
     #/ how we want to handle depth in this function
     #it will be very important
     def bestAction(self):
-        return random.randint(0, len(actions))
+        return random.randint(0, len(self.actions))
 
