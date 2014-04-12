30534b148a069e085dfd27015e762842e0ff596e
neppinger <nathanepp21@gmail.com>
2014-04-12 14:15:39 -0400

    Created initial action, goal list and added some key functions into Tribute.py

--- engine.py
+++ engine.py
@@ -6,7 +6,7 @@ import random
 import map
 from action import Action
 from tribute import Tribute
-
+from goal import Goal
 
 class GameEngine(object):
     """
@@ -20,17 +20,14 @@ class GameEngine(object):
     def start():
         me = GameEngine
         #create actions right now just moves
-        move_up = Action(2, '', 1, 0, (0, -1))
-        move_down = Action(2, '', 1, 1, (0, 1))
-        move_right = Action(2, '', 1, 2, (1, 0))
-        move_left = Action(2, '', 1, 3, (-1, 0))
+        actions = me.createActions()
+        goals = me.createGoals()
 
         #create the goals here
         #not really needed right now
 
         init_locations = [(x, y) for x in range(50) for y in range(50)]
         #creates all the actions
-        actions = [move_up, move_down, move_right, move_left]
         for i in range(0, 11):
             location = random.choice(init_locations)
             init_locations.remove(location)
@@ -38,7 +35,7 @@ class GameEngine(object):
 
         me.dims = (50, 50)
         me.view = graphics.GameView(*me.dims)
-        me.map = map.Map('maps/field.bmp')
+        me.map = map.Map('maps/field.png')
         me.state = me.map.seed_game_state(me.tributes)  # game.GameState()
         me.is_looping = True
         while me.is_looping and GameEngine.loop():
@@ -73,3 +70,42 @@ class GameEngine(object):
     def doAction(action):
         #Need to apply the action here
         pass
+
+    def createActions():
+        #Move action
+        move_up = Action([], '', 1, 0, (0, -1))
+        move_down = Action([], '', 1, 1, (0, 1))
+        move_right = Action([], '', 1, 2, (1, 0))
+        move_left = Action([], '', 1, 3, (-1, 0))
+
+        hunt = Action([5], ["hunger"], 1, 4,(0,0))
+
+        fight = Action([5], ["kill"], 1, 5, (0,0))
+
+        scavenger = Action([3], ["getweapon"], 1, 6, (0,0))
+        craft = Action([4], ["getweapon"], 1, 7, (0,0))
+        hide = Action([5], ["hide"], 1, 8, (0,0))
+        getwater = Action([5], ["hide"], 1, 9, (0,0))
+        rest = Action([5], ["rest"], 1, 10, (0,0))
+        talkAlly = Action([5], ["ally"], 1, 11, (0,0))
+
+        return [move_up, move_down, move_right, move_left, hunt, fight, scavenger,
+                craft, hide, getwater, rest, talkAlly]
+
+
+    def createGoals():
+        #Just giving default values for now
+        #Will figure out exact values later
+        #Starting these at zero and plan to increment every turn
+        hunger = Goal("hunger", 0)
+        thirst = Goal("thirst", 0)
+        rest = Goal("rest", 0)
+
+        #Is going to need to be changed somehow based on attributes
+        kill = Goal("kill", 5)
+        hide = Goal("hide", 5)
+
+        #Also going to be dependent on the value of certain things
+        getweapon = Goal("getweapon", 5)
+        ally = Goal("ally", 5)
+        return [hunger, thirst, rest, kill, hide, getweapon, ally]
--- tribute.py
+++ tribute.py
@@ -7,7 +7,6 @@ class Particle(object):
         self.state = state
         self.width, self.height = width, height
 
-
 class Tribute(Particle):
     #Goals = list of goals for tribute
     #actions = list of possible actions tribute can do
@@ -15,18 +14,47 @@ class Tribute(Particle):
         Particle.__init__(self, (x, y), 1, 1)
         self.goals = goals
         self.actions = actions
+        self.ret = []
 
     #Need to figure out exactly how far
     #/ how we want to handle depth in this function
     #it will be very important
-    def best_action(self):
+    def best_action(self, depth, maxdepth, actionName, ret):
         index = random.randint(0, len(self.actions) - 1)
-        return self.actions[index]
+
+        for action in self.actions:
+            if depth == maxdepth:
+                ret.append((self.calcDisc(),action))
+            else:
+                #Apply action to world copy and update and go one depth farther in
+                self.applyAction(action)
+                if depth == 0:
+                    self.best_action(depth+1, 4, action, ret)
+                else:
+                    self.best_action(depth+1, 4, actionName, ret)
+
+
 
     def act(self):
-        action = self.best_action()
+        #this function will have to be customized for each action
+        del self.ret[:]
+        self.best_action(0,4, '', self.ret)
+        bestVal = 1000000
+        for pairs in self.ret:
+            if pairs[0] < bestVal:
+                bestVal = pairs[0]
+                action = pairs[1]
+        
         self.state = ((self.state[0] + action.delta_state[0]) % engine.GameEngine.dims[0], (self.state[1] + action.delta_state[1]) % engine.GameEngine.dims[1])
 
+    #Action will update the state of the world by calculating
+    #Goal updates and where it is / fuzzy logic of where other tributes are
+    #will update current selfs world.
+    def applyAction(self, action):
+        return []
 
-
-
+    def calcDisc(self):
+        val = 0
+        for goal in self.goals:
+            val += goal.value*goal.value
+        return val
