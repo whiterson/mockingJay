db2c8de172ca6cc697aa56072fc32d992528888b
neppinger <nathanepp21@gmail.com>
2014-04-24 23:20:56 -0400

    changed size of display and added some initial tribute print statments

--- graphics.py
+++ graphics.py
@@ -20,7 +20,7 @@ class GameView(object):
         self.layers['particle'][0].set_colorkey((255, 0, 255))
         self.layers['particle'][0].fill((255, 0, 255))
 
-    def draw(self):
+    def draw(self, tribute, tributes):
         self.layers['particle'][0].fill((255, 0, 255))
         if bool(self.state):
             for layer in self.layers:
@@ -32,12 +32,49 @@ class GameView(object):
         for layer in layers_ordered:
             self.screen.blit(layer[1][0], (0, 0))
 
+        self.textTribute(tribute)
+        self.textStats(tributes)
         pygame.display.flip()
 
-    def render(self, gs):
+    def render(self, gs, tribute, tributes):
         """
         here, we set the game state to render
         @param gs: the game state to render
         """
         self.state = gs
-        self.draw()
+        self.draw(tribute, tributes)
+
+    def textStats(self, tributes):
+        fontobject=pygame.font.SysFont('Arial', 18)
+        sY = 520
+        sX = 70
+        for i in range(2, 4):
+            if i%2==0:
+                self.screen.blit(fontobject.render('District ' + str(i/2), 1, (255,255,255)), (sX, sY))
+                if (i-2)%4==0 and i!=2:
+                    sY+= 40
+                    sX-=800
+            #Check if alive here
+            print sX
+            print sY
+            self.screen.blit(fontobject.render(tributes[i-2].first_name + ' - Alive', 1, (255, 255, 255)), (sX-15, sY+25))
+            #Print weapons here
+            #Print alive here
+            sY+= 20
+            if i%2 == 0 and i!=2:
+                sX+=200
+                sY -= 40
+
+    def textTribute(self, tribute):
+        fontobject=pygame.font.SysFont('Arial', 18)
+        self.screen.blit(fontobject.render((tribute.first_name+ ' ' + tribute.last_name), 1, (255, 255, 255)), (510, 20))
+        self.screen.blit(fontobject.render(('*****************************************'), 1, (255, 255, 255)), (505, 55))
+        self.screen.blit(fontobject.render(('Size: ' +str(tribute.attributes['size'])), 1, (255, 255, 255)), (510, 80))
+        self.screen.blit(fontobject.render(('Strength: ' +str(tribute.attributes['strength'])), 1, (255, 255, 255)), (650, 80))
+        self.screen.blit(fontobject.render(('Speed: ' +str(tribute.attributes['speed'])), 1, (255, 255, 255)), (510, 120))
+        self.screen.blit(fontobject.render(('Hunting: ' +str(tribute.attributes['hunting_skill'])), 1, (255, 255, 255)), (650, 120))
+        self.screen.blit(fontobject.render(('Fighting: ' +str(tribute.attributes['fighting_skill'])), 1, (255, 255, 255)), (510, 160))
+        self.screen.blit(fontobject.render(('Camo Skill: ' +str(tribute.attributes['camouflage_skill'])), 1, (255, 255, 255)), (650, 160))
+        self.screen.blit(fontobject.render(('Friendliness: ' +str(tribute.attributes['friendliness'])), 1, (255, 255, 255)), (510, 200))
+        self.screen.blit(fontobject.render(('Stamina: ' +str(tribute.attributes['stamina'])), 1, (255, 255, 255)), (650, 200))
+        self.screen.blit(fontobject.render(('Endurance: ' +str(tribute.attributes['endurance'])), 1, (255, 255, 255)), (510, 240))
