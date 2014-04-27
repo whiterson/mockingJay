43ab80bd239fff847d80ffd878c56590685dc7d9
Whitney H <henderson.whit@gmail.com>
2014-04-27 15:36:51 -0400

    Added new Crafting Types

--- locationDef.py
+++ locationDef.py
@@ -6,15 +6,21 @@ class locationDef(object):
         self.playerThere = False    #True/False
         self.tribute = None         #Tribute that is in a space. Null otherwise
         self.terrain = 0            #0-15 Value for terrain Type
-        self.foodChance = 1         #integer value for probability of food
-        self.waterChance = 1        #integer value for probability of water
+        self.foodChance = 0         #integer value for probability of food
+        self.waterChance = 0        #integer value for probability of water
         self.visibility = 0         #integer value for visibility
-        self.stickChance = 1        #for crafting: Integer Value probability of stick
-        self.sharpStoneChance = 1   #for crafting: integer value probablity of sharp stone
-        self.featherChance = 1      #for crafting: integer value probablilty of feather
-        self.vineChance = 1         #for crafting: integer value probablity of vine
+        self.shortStickChance = 0   #for crafting: Integer Value probability of stick
+        self.sharpStoneChance = 0   #for crafting: integer value probablity of sharp stone
+        self.featherChance = 0      #for crafting: integer value probablilty of feather
+        self.vineChance = 0         #for crafting: integer value probablity of vine
         self.speedChange = 0        #integer value movement speed changes
-        self.weaponChance = 0       #inter value for probability of weapon
+        self.weaponChance = 0        #inter value for probability of weapon
+        self.longStickChance = 0
+        self.broadStoneChance = 0
+        self.longGrassChance = 0
+        self.reedsChance = 0
+        self.pebblesChance = 0
+        self.thornsChance = 0
 
     ########### GETS ##############
 
@@ -36,7 +42,7 @@ class locationDef(object):
     def getWaterChance(self):
         return self.waterChance
 
-    def getStickChance(self):
+    def getShortStickChance(self):
         return self.stickChance
 
     def getVisibility(self):
@@ -57,6 +63,24 @@ class locationDef(object):
     def getWeaponChance(self):
         return self.weaponChance
 
+    def getLongStickChance(self):
+        return self.longStickChance
+
+    def getBroadStoneChance(self):
+        return self.broadStoneChance
+
+    def getLongGrassChance(self):
+        return self.longGrassChance
+
+    def getReedsChance(self):
+        return self.reedsChance
+
+    def getPebblesChance(self):
+        return self.pebblesChance
+
+    def getThornsChance(self):
+        return self.thornsChance
+
     ########### SETS ##############
     def setStartSpace(self, input):
         self.startSpace = input
@@ -76,7 +100,7 @@ class locationDef(object):
     def setWaterChance(self, input):
         self.waterChance = input
 
-    def setStickChance(self, input):
+    def setShortStickChance(self, input):
         self.stickChance = input
 
     def setVisibility(self, input):
@@ -96,3 +120,21 @@ class locationDef(object):
 
     def setWeaponChance(self, input):
         self.weaponChance = input
+
+    def setLongStickChance(self, input):
+        self.longStickChance = input
+
+    def setBroadStoneChance(self, input):
+        self.broadStoneChance = input
+
+    def setLongGrassChance(self, input):
+        self.longGrassChance = input
+
+    def setReedsChance(self, input):
+        self.reedsChance = input
+
+    def setPebblesChance(self, input):
+        self.pebblesChance = input
+
+    def setThornsChance(self, input):
+        self.thornsChance = input
